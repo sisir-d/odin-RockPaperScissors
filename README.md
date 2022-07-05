2e1c8eb84a7659c53c85c91a9470d1b2fb9eb00d
sisir <sisir7660@gmail.com>
2022-07-05 21:36:27 +0530

    complete the project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # odin-RockPaperScissors
+
+The odin project's 3rd project after learning basic javascript
+
+Here the preview:
+
+https://sisir-d.github.io/odin-RockPaperScissors/
---
+++ main.js
@@ -0,0 +1,56 @@
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  // computer choice function
+  function computerPlay() {
+    let choices = ["rock", "paper", "scissors"];
+    let choiceNumber = Math.floor(Math.random() * choices.length);
+    return choices[choiceNumber];
+  }
+
+  // play round game
+  function playRound(playerSelection, computerSelection = computerPlay()) {
+    let pS = playerSelection.toLowerCase();
+    let cS = computerSelection;
+    let msg = "";
+
+    // tie
+    if (pS === cS) {
+      return "It's a TIE!";
+    }
+
+    // winner // loser
+    if ((pS == "rock") & (cS == "scissors")) {
+      msg += "You Win! Rock beats scissors";
+      playerScore++;
+    } else if ((pS == "scissors") & (cS == "paper")) {
+      msg += "You Win! Scissors beats Paper";
+      playerScore++;
+    } else if ((pS == "paper") & (cS == "rock")) {
+      msg += "You Win! Paper beats Rock";
+      playerScore++;
+    } else {
+      msg += `You Lose! ${cS} beats ${pS}`;
+      computerScore++;
+    }
+
+    return msg;
+  }
+
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Enter [rock, paper or scissors] : ");
+    console.log(playRound(playerSelection));
+  }
+
+  // game winner / loser
+  if (playerScore > computerScore) {
+    console.log("You Won this game!");
+  } else if (playerScore < computerScore) {
+    console.log("You Lost this game!");
+  } else {
+    console.log("This game is TiE!");
+  }
+}
+
+game();
